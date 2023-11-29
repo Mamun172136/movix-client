38a132cf808ad503db871a909cd598653c6cc66a
Mamun172136 <ma1536204@gmail.com>
2023-11-29 17:59:32 +0600

    done register page

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Button } from "antd";
+import "./stylesheets/allignments.css";
+import "./stylesheets/custom-components.css";
+import "./stylesheets/custom.css";
+import "./stylesheets/form-elements.css";
+import "./stylesheets/layout.css";
+import "./stylesheets/size.css";
+import "./stylesheets/text-elements.css";
+import "./stylesheets/theme.css";
+
+function App() {
+  return (
+    <div>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+
+import Home from "./pages/Home";
+// import App from './App.jsx'
+import "./index.css";
+
+import "./stylesheets/allignments.css";
+import "./stylesheets/custom-components.css";
+import "./stylesheets/custom.css";
+import "./stylesheets/form-elements.css";
+import "./stylesheets/layout.css";
+import "./stylesheets/size.css";
+import "./stylesheets/text-elements.css";
+import "./stylesheets/theme.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register></Register>} />
+        <Route path="/" element={<Home></Home>} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
