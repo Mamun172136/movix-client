f416729b1924c6a9c5af086d4688683cc50aa62b
Mamun172136 <ma1536204@gmail.com>
2023-12-01 20:45:07 +0600

    done tabs in admin page

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,7 +1,21 @@
+import { Tabs } from "antd";
 import React from "react";
 
 const Admin = () => {
-  return <div>admin</div>;
+  return (
+    <div>
+      <h2>admin</h2>
+      <Tabs defaultActiveKey="1">
+        <Tabs.TabPane tab="Movies" key="1">
+          movies
+        </Tabs.TabPane>
+
+        <Tabs.TabPane tab="Theatres" key="2">
+          theater
+        </Tabs.TabPane>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Admin;
